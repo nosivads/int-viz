f442bd38cc6572fd7006553173b978d23ad8e836
Stephen Davison <stephen.davison@cgu.edu>
2023-05-12 13:20:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,17 @@ continent_list = list(df['continent'].unique())
 metric_list = list(df['metric'].unique())
 
 with st.sidebar:
+    st.subheader("Configure the plot")
     continent = st.selectbox(label='Choose a continent', options=continent_list)
     metric = st.selectbox(label='Choose a metric', options=metric_list)
 
 query = f"continent=='{continent}' & metric=='{metric}'"
 
-df_gdp_o = df.query(query)
-fig = px.line(df_gdp_o, x='year', y='value', color='country', title='GDP for countries in Oceania')
+df_gdp = df.query(query)
+
+metric_labels = {'gdpPercap' : 'GDP Per Capita', 'lifeExp' : 'Average Life Expectancy', 'pop' : 'Population'}
+
+title = f"{metric_labels[metric]} for countries in {continent}"
+fig = px.line(df_gdp, x='year', y='value', color='country', title=title)
 
 st.plotly_chart(fig, use_container_width=True)
